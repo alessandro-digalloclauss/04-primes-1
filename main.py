fd6858f9c8e1d8cd6f217a86c785f219fba17e67
alessandro-digalloclauss <alessandrodigalloclauss@gmail.com>
2025-09-22 12:57:08 +0000

    fonction nombre premier

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ from math import sqrt
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
+    if p <= 1:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    for x in [2, 4, 17, 1, 58]:
+        print(f"isprime({x}):", isprime(x))
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
@@ -23,5 +22,4 @@ def main():
     print()
 
 
-if __name__ == "__main__":
-    main()
+main()
